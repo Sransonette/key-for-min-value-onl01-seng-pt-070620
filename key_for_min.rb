78f1c61b124d7bcd38610979339ac46f0f0b1c9b
Sean Ransonette <sransonette@gmail.com>
2020-07-17 04:56:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,12 @@
 def key_for_min_value(name_hash)
   min_value = nil
   min_key = nil
-  name_hash.each do |x, value|
+  name_hash.each do |key, value|
     if value == 0 || value < min_value
       min_value = value
-      min_key = x
+      min_key = key
     end
   end
   min_key
 end
+
